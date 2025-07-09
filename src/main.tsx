7089f3c2d4ab241e223ce6181d0af5d0ffa6d7b9
Samuel Forrest <sam@samuelforrest.me>
2025-07-09 16:52:59 +0100

    globa

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log('Main.tsx is loading...');
+console.log('Root element:', document.getElementById("root"));
+
 createRoot(document.getElementById("root")!).render(<App />);
