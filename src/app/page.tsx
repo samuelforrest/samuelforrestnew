75f2633e9053efd16c2a78fb2f51f7a6739f5ce8
Samuel Forrest <sam@samuelforrest.me>
2025-08-26 20:33:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -245,16 +245,21 @@ export default function Page() {
       </section>
       
       {/* Credit */}
-      <section className="pb-4 mb-4">
-        <BlurFade delay={BLUR_FADE_DELAY * 18}>
-          <div className="text-center">
-            <p className="text-xs text-muted-foreground/70">Website modified from Oluwatobi Salawu&apos;s<Link
-                  href="https://github.com/tobilobasalawu"
-                  className="text-blue-500 hover:underline"
-                >Github</Link>
-          </div>
-        </BlurFade>
-      </section>
+<section className="pb-4 mb-4">
+  <BlurFade delay={BLUR_FADE_DELAY * 18}>
+    <div className="text-center">
+      <p className="text-xs text-muted-foreground/70">
+        Website modified from Oluwatobi Salawu&apos;s{' '}
+        <Link
+          href="https://github.com/tobilobasalawu"
+          className="text-blue-500 hover:underline"
+        >
+          Github
+        </Link>
+      </p>
+    </div>
+  </BlurFade>
+</section>
     </main>
   );
 }
